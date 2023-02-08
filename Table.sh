bdb6c865036d3321156693815cddef244d42ef2f
Ashish Chouhan <ashishchouhan2000.11@gmail.com>
2023-02-08 12:55:43 +0530

    Argument from command line implemented

--- Table.sh
+++ Table.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# GETTING USER INPUT
-read -p "Enter a number to display its table " num
-
 # CHECKING THAT THE NUMBER IS VALID OR NOT
-if ! [[ $num =~ ^[0-9]+ ]]
+if ! [[ $1 =~ ^[0-9]+ ]]
 then
     echo "Enter a valid number"
     exit 1
@@ -16,6 +13,6 @@ multiplier=1
 # LOOP TO PRINT THE TABLE
 while [[ $multiplier -lt 11 ]]
 do
-    echo "$num X $multiplier = $(($num*$multiplier))"
+    echo "$1 X $multiplier = $(($1*$multiplier))"
     ((multiplier++))
 done
